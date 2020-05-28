3fb3c4b1a6778eeb2c014fcf0ebe8390b89a6884
Julian Bensch <Bensch.Julian@gmail.com>
2020-05-28 18:54:09 +0200

    Added exception handling and a browser for maze files (launcher)

---
+++ Launcher.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using MazeGame;
+
+namespace mazegame
+{
+    public partial class MazeRunner : Form
+    {
+        private String filePath;
+        [STAThread]
+        static void Main() {
+            Application.Run(new MazeRunner());
+        }
+
+        public MazeRunner() {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e) {
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            String pathOfExecutable = System.Environment.CurrentDirectory + "/";
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.InitialDirectory = pathOfExecutable;
+            openFileDialog.Filter = "maze files (*.maze)|*.maze";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK) {
+                //Get the path of specified file
+                tickImage.Visible = true;
+                filePath = openFileDialog.FileName;
+                button2.Enabled = true;
+            }
+        }
+        void button2_Click(object sender, EventArgs e) {
+            Hide();
+            Form gameForm = new MazeGame.MazeGame(this, filePath);
+            try {
+                gameForm.ShowDialog();
+            } catch {
+
+            }
+        }
+    }
+}
---
+++ MazeReadException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mazegame
+{
+    class MazeReadException : Exception
+    {
+        public MazeReadException() {
+        }
+
+        public MazeReadException(string message)
+            : base(message) {
+        }
+
+        public MazeReadException(string message, Exception inner)
+            : base(message, inner) {
+        }
+    }
+}
